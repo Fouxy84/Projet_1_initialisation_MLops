2df0ccfaaa0d107a2c2c4d140db5c64a1363cbef
Adam F <151775657+Fouxy84@users.noreply.github.com>
2026-02-12 23:01:08 +0100

    training update

--- api/main.py
+++ api/main.py
@@ -1,2 +1,13 @@
 #main.py
-api = "Hello World"
+import mlflow
+import mlflow.sklearn
+import mlflow.pyfunc
+# enregistrement du meilleur modèle final
+best_model_name = results_df_grid.iloc[0]["model"]
+print("Best model:", best_model_name)
+final_model = best_models[best_model_name]
+mlflow.sklearn.log_model(final_model,name="model",registered_model_name=f"HomeCredit_Scoring_final_{best_model_name.replace(' ', '_')}")
+i
+
+model_name = "HomeCredit_Scoring_final_XGBoost"
+model = mlflow.pyfunc.load_model(f"models:/{model_name}/Production")
