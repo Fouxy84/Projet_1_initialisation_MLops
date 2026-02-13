a065142ece95d90fa153c72401c388645c578a3a
Adam F <151775657+Fouxy84@users.noreply.github.com>
2026-02-13 13:40:06 +0100

    update api main

--- api/main.py
+++ api/main.py
@@ -1,35 +1,29 @@
 # api/main.py
+import joblib
 import mlflow
 import mlflow.pyfunc
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path
 import pandas as pd
-
+from inference_data import ClientData
 import time
 from pydantic import BaseModel
 
 app = FastAPI(title="HomeCredit Scoring API")
 
-MODEL_PATH = "file:///C:/Users/coach/Desktop/datascientest/OpenClassrooms/Projects_MLops/Projet_1_initialisation_MLops/notebook/mlruns/models/HomeCredit_Scoring_final_LightGBM/version-5"
-MODEL_PATH = r"C:\Users\coach\Desktop\datascientest\OpenClassrooms\Projects_MLops\mlruns-20260212T220513Z-1-001\mlruns\models\HomeCredit_Scoring_final_LightGBM\version-5"
-
+#MODEL_PATH = "file:///C:/Users/coach/Desktop/datascientest/OpenClassrooms/Projects_MLops/Projet_1_initialisation_MLops/notebook/mlruns/models/HomeCredit_Scoring_final_LightGBM/version-5"
+#MODEL_PATH = r"C:\Users\coach\Desktop\datascientest\OpenClassrooms\Projects_MLops\mlruns-20260212T220513Z-1-001\mlruns\models\HomeCredit_Scoring_final_LightGBM\version-5"
 #model = mlflow.pyfunc.load_model(MODEL_PATH)
+
+BASE_DIR = Path(r"C:/Users/coach/Desktop/datascientest/OpenClassrooms/Projects_MLops/Projet_1_initialisation_MLops")
+MODEL_PATH = BASE_DIR / "models" / "lightgbm_model.joblib"
+
+artifact = joblib.load(MODEL_PATH)
+model = artifact["model"]
+features = artifact["features"]
+imputer = artifact["imputer"]
 
 best_thresold = 0.5
 
-# ---- Schema Pydantic ----
-class ClientData(BaseModel):
-    AMT_INCOME_TOTAL: float
-    AMT_CREDIT: float
-    AMT_ANNUITY: float
-    DAYS_EMPLOYED: float
-    DAYS_BIRTH: float
-    EXT_SOURCE_1: float
-    EXT_SOURCE_2: float
-    EXT_SOURCE_3: float
-    NAME_INCOME_TYPE: str
-    NAME_EDUCATION_TYPE: str
-    NAME_FAMILY_STATUS: str 
-    NAME_HOUSING_TYPE: str
 
 @app.get("/")
 def health():
@@ -40,10 +34,10 @@ def predict(data: ClientData):
     try:
         start_time = time.time()
         input_df = pd.DataFrame([data.dict()])
-        proba = 0.2 #model.predict(input_df)[0]
+        input_df = imputer.transform(input_df)
+        proba = model.predict_proba(input_df)[0][1]
         prediction = int(proba >= best_thresold)
         latency = time.time() - start_time
-        
         return {"prediction_probability": float(proba),"prediction": prediction,"latency_seconds": round(latency, 4)}
     
     except Exception as e:
