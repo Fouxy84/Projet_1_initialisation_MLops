9bf966332401f1c8b06a1d1d23c5847683a85201
Adam F <151775657+Fouxy84@users.noreply.github.com>
2026-03-06 14:11:44 +0100

    app.py

--- app.py
+++ app.py
@@ -1,44 +1,7 @@
-
 import gradio as gr
-from fastapi import FastAPI
-import requests
-import os
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-GITHUB_TOKEN = os.getenv("REMOVED")
-=======
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
->>>>>>> 8f421e6 (update dashbord token access)
-REPO = "Fouxy84/Projet_1_initialisation_MLops"
-=======
-# ==========================
-# FASTAPI BACKEND
-# ==========================
-api = FastAPI()
->>>>>>> d3dc82c (update dashbord app)
+def greet(name):
+    return "Hello " + name + "!!"
 
-@api.get("/")
-def greet_json():
-    return {"Hello": "World!"}
-
-
-# ==========================
-# GRADIO FRONTEND
-# ==========================
-def call_api():
-     return greet_json()
-
-
-with gr.Blocks() as demo:
-    gr.Markdown("# 🚀 FastAPI + Gradio Demo")
-
-    btn = gr.Button("Call FastAPI")
-
-    output = gr.JSON()
-
-    btn.click(call_api, outputs=output)
-
-
-
-app = gr.mount_gradio_app(api, demo, path="/")
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo.launch()
